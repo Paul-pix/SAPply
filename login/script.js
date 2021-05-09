93ce047fed7b5920b00c26d9e99ac3efef100036
Paul-pix <72570334+Paul-pix@users.noreply.github.com>
2021-05-09 21:50:38 +0200

    Initial commit

---
+++ login/script.js
@@ -0,0 +1,9 @@
+function validaForm(){
+    if(document.loginForm.remember.checked){
+        alert("Salvato per i prossimi login");
+    }
+    else{
+    alert("Hai deciso di non ricordarti per i prossimi login");s
+    }
+
+}
