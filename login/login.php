c1f2ac7eefab29a74d91035ad894f29163fade05
Paul-pix <72570334+Paul-pix@users.noreply.github.com>
2021-05-10 16:25:04 +0200

    cambio lingua

--- login/login.php
+++ login/login.php
@@ -2,14 +2,14 @@
     <head></head>
     <body>
         <?php
-        $dbconn=pg_connect("host=localhost port=5432 dbname=Login suer=postgres password=admin")
+        $dbconn=pg_connect("host=localhost port=5432 dbname=Login user=postgres password=admin")    // connessione database
                 or die('Could not connect: '. pg_last_error());
-                if(!(isset($_POST['loginButton']))) {
-                        header("Location: ../index.html");
+                if(!(isset($_POST['loginButton']))) {           // contollo se la variabile per il loginButton della chiamata POST esiste 
+                        header("Location: ../index.html");      //se accedo a login.php senza passare dal login button vengo reindirizzato su index.html
                 }
                 else{
-                    $email=$_POST['inputEmail'];
-                    $q1="select * from utente where email=$1";
+                    $email=$_POST['inputEmail'];                //salvo inputEmail nella variabile email
+                    $q1="select * from utente where email=$1";      
                     $result=pg_query_params($dbconn,$q1,array($email));
                     if(!($line=pg_fetch_array($result,null,PGSQL,ASSOC))){
                         echo "<h1> You are not a registered user </h1> <a href=../registrazione/index.html>
