d3a7305853558394eb778669faa48fc5208ea035
Paul-pix <72570334+Paul-pix@users.noreply.github.com>
2021-05-11 13:04:53 +0200

    aggiunto validate Registration php

--- registrazione/script.js
+++ registrazione/script.js
@@ -1,4 +1,4 @@
-function validaForm(){
+function validaForm(){       
     if(controllaCAP()){
         if(document.myForm.remember.checked){
             window.alert("Hai scelto di ricordarti per i prossimi accessi");
